2d3a23d15f32e5b5b3c5afa53645d4ae053e1e77
Gaurav Tak <gauravtak996@gmail.com>
2024-12-16 23:57:03 +0530

    extending input

--- token/token.go
+++ token/token.go
@@ -18,6 +18,12 @@ const (
 	//Operators
 	ASSIGN = "="
 	PLUS = "+"
+	MINUS = "-"
+	BANG = "!"
+	ASTERISK = "*"
+	SLASH = "/"
+	LT = "<"
+	GT = ">"
 
 	//Delimiters
 	COMMA = ","
@@ -28,7 +34,33 @@ const (
 	LBRACE = "}"
 	RBRACE = "{"
 
+	EQ = "=="
+	NOT_EQ = "!="
+
 	//Keywords
 	FUNCTION = "FUNCTION"
 	LET = "LET"
+	TRUE = "TRUE"
+	FALSE = "FALSE"
+	IF = "IF"
+	ELSE = "ELSE"
+	RETURN = "RETURN"
 )
+
+var keywords = map[string]TokenType{
+	"fn": FUNCTION,
+	"let": LET,
+	"true": TRUE, 
+	"false": FALSE,
+	"if": IF, 
+	"else": ELSE,
+	"return": RETURN,
+}
+
+// ident is short for identifier
+func LookupIdent(ident string) TokenType{
+	if tok, ok := keywords[ident]; ok {
+		return tok
+	}
+	return IDENT
+}
