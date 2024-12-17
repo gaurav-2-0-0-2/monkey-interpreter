47d4a608a0c7d6beb7e8d2227b703f4dbdf28122
Gaurav Tak <gauravtak996@gmail.com>
2024-12-17 22:45:34 +0530

    adding repl

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -43,7 +43,6 @@ func (l *Lexer) NextToken() token.Token {
 		}else {
 			tok = newToken(token.BANG, l.ch)
 		}
-		tok = newToken(token.BANG, l.ch)
 	case '/':
 		tok = newToken(token.SLASH, l.ch)
 	case '*':
